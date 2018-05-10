0c9c1470bc3a551a7edd8e023f1aa5b4996baf70
Muhammad Abdul Aziz <zizcode.exporadev@gmail.com>
2018-05-11 04:50:24 +0700

    Update firebase.js

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: "AIzaSyA2G9HSbYFMvv3eRu8imXN3WrGtqC_H8JM",
-	authDomain: "find-answer.firebaseapp.com",
-	databaseURL: "https://find-answer.firebaseio.com",
-	projectId: "find-answer",
-	storageBucket: "find-answer.appspot.com",
-	messagingSenderId: "1090502266839"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: ""
 }
 
 firebase.initializeApp(config);
